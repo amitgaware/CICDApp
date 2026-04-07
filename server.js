08deaef4fca92c89fcd532e9b25f6a52b6095f53
amitgaware <amitgaware@gmail.com>
2026-04-07 20:35:55 +0530

    Add Prometheus enable code

--- server.js
+++ server.js
@@ -2,10 +2,36 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('HELLO WORLD!!!');
+// 🔽 Prometheus setup
+const client = require('prom-client');
+
+// collect default metrics (CPU, memory, etc.)
+client.collectDefaultMetrics();
+
+// custom metric: count HTTP requests
+const httpRequests = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of requests',
 });
 
+// middleware to count every request
+app.use((req, res, next) => {
+  httpRequests.inc();
+  next();
+});
+
+// your existing route
+app.get('/', (req, res) => {
+  res.send('HELLO WORLD!!!');
+});
+
+// 🔽 metrics endpoint for Prometheus
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+// start server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
